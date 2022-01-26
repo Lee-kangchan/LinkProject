0ed35aedf3a6afbe6075b449b5121ea1ce013b3b
Lee-kangchan <lsc4237@gmail.com>
2022-01-26 22:23:57 +0900

    [add] Update, Sign Test 추가 (추후 exception 체크 수정)

--- backend/src/test/java/com/chan/link/user/UserSignTest.java
+++ backend/src/test/java/com/chan/link/user/UserSignTest.java
@@ -4,6 +4,8 @@ import com.chan.link.domain.user.dto.UserDto;
 import com.chan.link.domain.user.repository.UserRepository;
 import com.chan.link.domain.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +17,9 @@ public class UserSignTest {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
-
     private UserDto.Sign signDto;
 
+    private UserDto.Sign alreadySignDto;
     @BeforeEach
     public void before() {
         signDto = UserDto.Sign.builder()
@@ -31,5 +31,32 @@ public class UserSignTest {
                 .phone("010-0011-1112")
                 .build();
 
+        alreadySignDto = UserDto.Sign.builder()
+                .email("admin@admin.com")
+                .pw("1111")
+                .gender("men")
+                .nickname("testMen")
+                .name("test")
+                .phone("010-0011-1112")
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void signSuccessTest(){
+        userService.signService(signDto);
+    }
+    @Test
+    @DisplayName("회원가입 중복 처리")
+    void continuitySignTest(){
+        userService.signService(signDto);
+        userService.signService(signDto);
+    }
+
+    @Test
+    @DisplayName("가입된 이메일 회원가입")
+    void loginPasswordFailTest(){
+        userService.signService(alreadySignDto);
     }
 }
--- backend/src/test/java/com/chan/link/user/UserUpdateTest.java
+++ backend/src/test/java/com/chan/link/user/UserUpdateTest.java
@@ -3,9 +3,9 @@ package com.chan.link.user;
 import com.chan.link.domain.user.dto.UserDto;
 import com.chan.link.domain.user.repository.UserRepository;
 import com.chan.link.domain.user.service.UserService;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,9 +17,7 @@ public class UserUpdateTest {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private UserRepository userRepository;
-
+    private UserDto.Update alreadyUpdateDto;
     private UserDto.Update updateDto;
 
     @BeforeEach
@@ -27,8 +25,29 @@ public class UserUpdateTest {
         updateDto = UserDto.Update.builder()
                 .gender("women")
                 .pw("2222")
+                .name("test2")
+                .nickname("test2")
                 .phone("010-0000-1111")
                 .build();
+        alreadyUpdateDto = UserDto.Update.builder()
+                .gender("men")
+                .pw("1111")
+                .phone("010-0000-0000")
+                .name("test")
+                .nickname("test")
+                .build();
+    }
+    @Test
+    @DisplayName("유저 정보 변경 성공")
+    void userUpdateSuccessTest(){
+        userService.userUpdateService(updateDto);
     }
 
+
+    @Test
+    @DisplayName("똑같은 유저 정보 입력")
+    void equalsUserUpdateTest(){
+        userService.userUpdateService(alreadyUpdateDto);
+    }
 }
+
